f31acf2774c32f6dcb9fcbabdd37ea91adb126b9
John Brayton <jbrayton@goldenhillsoftware.com>
2017-02-11 13:24:07 -0500

    works end-to-end

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 gem 'highline'
 gem 'httpclient', '~> 2.8.3'
 gem 'ice_cube', '~> 0.15.0'
+gem 'oga', '~> 2.9'
--- repeating.rb
+++ repeating.rb
@@ -2,17 +2,20 @@ require 'highline/import'
 require 'httpclient'
 require "ice_cube"
 require 'io/console'
+require 'oga'
 require 'yaml'
 
-URL_PREFIX = "http://cocoaheads.org:8106/admin/"
-LOGIN_URL = "http://cocoaheads.org:8106/admin/index.pl"
+URL = "http://cocoaheads.org:8106/admin/"
 
+DISPLAY_DATE_FORMAT = "%A, %B %d, %Y"
+DISPLAY_TIME_FORMAT = "%l:%M %p"
+DATE_INPUT_FORMAT = "%Y-%m-%d %H:%M"
 
 ###
 # Ask the user to enter the cocoaheads.org password. Returns the entered password.
 ###
 def prompt_for_password(config)
-	print "URL: #{URL_PREFIX}\n"
+	print "URL: #{URL}\n"
 	print "Username: #{config['username']}\n"
 	print "Password: "
 	password = STDIN.noecho(&:gets).chomp
@@ -26,7 +29,7 @@ end
 ###
 def login(config, http_client, password) 
 	post_args =  { 'runnum' => '2', 'username' => config['username'], 'password' => password }
-	login_response = http_client.post(LOGIN_URL, post_args)
+	login_response = http_client.post(URL, post_args)
 	if login_response.content.include? "<h2>Please login:</h2>"
 		puts "Unable to log in, probably because the username/password combination was incorrect."
 		exit
@@ -66,11 +69,11 @@ def display_confirmation(config)
 	puts "Meeting Dates:"
 	repeat_dates = get_repeat_dates(config)
 	repeat_dates.each do |d|
-		puts "• " + start_time(config, d).strftime("%A, %B %d, %Y")
+		puts "• " + start_time(config, d).strftime(DISPLAY_DATE_FORMAT)
 	end
 	puts
-	puts "Start time: " + Time.parse(config['start_time']).strftime("%l:%M %p").strip
-	puts "End time: " + Time.parse(config['end_time']).strftime("%l:%M %p").strip
+	puts "Start time: " + Time.parse(config['start_time']).strftime(DISPLAY_TIME_FORMAT).strip
+	puts "End time: " + Time.parse(config['end_time']).strftime(DISPLAY_TIME_FORMAT).strip
 	puts 
 	puts "Location: #{config['location_string']}"
 	puts 
@@ -84,9 +87,57 @@ def display_confirmation(config)
 	exit unless confirm.downcase == 'y'
 end
 
+###
+# Retrieves the Group ID. This is needed to add the meetings.
+###
+def get_group_id(config, http_client)
+	puts
+	puts "Retrieving the Group ID…"
+	response = http_client.get(URL, { 'rm' => 'scheduleeditor' })
+	document = Oga.parse_html response.content
+	group_id_element = document.at_xpath("//input[@name='groupid']")
+	if group_id_element.nil?
+		puts "Unable to find the Group ID."
+		exit
+	end
+	value = group_id_element.attr('value')
+	if value.nil?
+		puts "Unable to find the Group ID."
+		exit
+	end
+	return value
+end
+
+###
+# Return the date in the format that cocoaheads.org expects it.
+###
+def get_ch_date(config, meeting_date, config_name)
+	config = config[config_name]
+	return Time.parse(config, meeting_date).strftime(DATE_INPUT_FORMAT)
+end
+
+###
+# Add meetings.
+###
+def add_meetings(config, http_client)
+	group_id = get_group_id(config, http_client)
+	repeat_dates = get_repeat_dates(config)
+	repeat_dates.each do |meeting_date|
+		display_date = meeting_date.strftime(DISPLAY_DATE_FORMAT)
+		puts "Adding #{display_date} meeting…"
+		start_date_string = get_ch_date(config, meeting_date, 'start_time')
+		end_date_string = get_ch_date(config, meeting_date, 'end_time')
+		args = { 'runnum' => '2', 'rm' => 'scheduleeditor', 'groupid' => group_id, 'eventid' => '', 'startdate' => start_date_string, 'enddate' => end_date_string, 'location' => config['location_string'], 'latitude' => config['latitude'], 'longitude' => config['longitude'], 'locationdetails' => config['meeting_details'], 'newmeeting' => 'Create New Meeting...' } 
+		http_client.post(URL, args)
+	end
+end
+
 config = YAML.load_file('config.yaml')
 http_client = HTTPClient.new
 display_confirmation(config)
 password = prompt_for_password(config)
 login(config, http_client, password)
-puts "done"
+add_meetings(config, http_client)
+puts "Done."
+puts
+puts "Important: This script has no way to validate that the meetings are added. Please verify through the user interface."
