b6d9751b4713a03ab7e5fcb861122d18e7cb1586
John Brayton <jbrayton@goldenhillsoftware.com>
2017-02-11 14:53:02 -0500

    done

--- script.rb
+++ script.rb
@@ -7,9 +7,17 @@ require 'yaml'
 
 URL = "http://cocoaheads.org:8106/admin/"
 
-DISPLAY_DATE_FORMAT = "%A, %B %d, %Y"
+# The format used by the script to display a date (without the time) to the user.
+DISPLAY_DATE_FORMAT = "%A, %B %e, %Y"
+
+# The format used by the script to display a time (without the date) to the user.
 DISPLAY_TIME_FORMAT = "%l:%M %p"
+
+# The format in which cocoaheads.org expects dates and times of meetings.
 DATE_INPUT_FORMAT = "%Y-%m-%d %H:%M"
+
+# The format in which cocoaheads.org presents dates and times of meetings.
+PAGE_DISPLAY_DATE_FORMAT = "%B %e, %Y %H:%M"
 
 ###
 # Ask the user to enter the cocoaheads.org password. Returns the entered password.
@@ -25,7 +33,7 @@ end
 
 ###
 # Log in to cocoaheads.org. Will exit the script if unable to confirm that the client is 
-# logged in. If this exits successfully, assume that the user is logged in.
+# logged in. If this exits successfully, the user is logged in.
 ###
 def login(config, http_client, password) 
 	post_args =  { 'runnum' => '2', 'username' => config['username'], 'password' => password }
@@ -40,7 +48,7 @@ def login(config, http_client, password)
 end
 
 ###
-# Returns a list of meeting dates based on the configuration.
+# Return a list of meeting dates based on the configuration.
 ###
 def get_repeat_dates(config)
 	start_date = Time.new(config['start_date_year'], Date::MONTHNAMES.index(config['start_date_month']), 1)
@@ -54,22 +62,14 @@ def get_repeat_dates(config)
 end
 
 ###
-# Return the start time (date + time) of a meeting, based on the day of the configured 
-# start time and the specified date.
-###
-def start_time(config, meeting_date) 
-	return Time.parse(config['start_time'], meeting_date)
-end
-
-###
-# Allow the user to confirm the information for the meetings.
+# Allow the user to confirm information for the meetings.
 ###
 def display_confirmation(config)
 	puts
 	puts "Meeting Dates:"
 	repeat_dates = get_repeat_dates(config)
 	repeat_dates.each do |d|
-		puts "• " + start_time(config, d).strftime(DISPLAY_DATE_FORMAT)
+		puts "• " + d.strftime(DISPLAY_DATE_FORMAT)
 	end
 	puts
 	puts "Start time: " + Time.parse(config['start_time']).strftime(DISPLAY_TIME_FORMAT).strip
@@ -88,14 +88,21 @@ def display_confirmation(config)
 end
 
 ###
-# Retrieves the Group ID. This is needed to add the meetings.
+# Retrieve and return the meeting page as a DOM document.
 ###
-def get_group_id(config, http_client)
+def get_meeting_page_document(config, http_client)
 	puts
-	puts "Retrieving the Group ID…"
+	puts "Retrieving the meetings page…"
 	response = http_client.get(URL, { 'rm' => 'scheduleeditor' })
-	document = Oga.parse_html response.content
-	group_id_element = document.at_xpath("//input[@name='groupid']")
+	return Oga.parse_html response.content
+end
+
+###
+# Retrieve the Group ID from the meeting page document.
+# This is needed to add the meetings.
+###
+def get_group_id(meeting_page_document)
+	group_id_element = meeting_page_document.at_xpath("//input[@name='groupid']")
 	if group_id_element.nil?
 		puts "Unable to find the Group ID."
 		exit
@@ -109,29 +116,53 @@ def get_group_id(config, http_client)
 end
 
 ###
+# Return true if a meeting for the specified date is found in the document.
+# Otherwise return false.
+###
+def get_meeting_date_found(config, meeting_page_document, meeting_date)
+	config = config['start_time']
+	formatted_date = Time.parse(config, meeting_date).strftime(PAGE_DISPLAY_DATE_FORMAT).gsub("  ", " ")
+	return !meeting_page_document.at_xpath("//input[@value='#{formatted_date}']").nil?
+end
+
+###
 # Return the date in the format that cocoaheads.org expects it.
 ###
-def get_ch_date(config, meeting_date, config_name)
+def get_ch_input_date(config, meeting_date, config_name)
 	config = config[config_name]
 	return Time.parse(config, meeting_date).strftime(DATE_INPUT_FORMAT)
 end
 
 ###
-# Add meetings.
+# Add meetings. Stop if adding a meeting appears to have failed. Tries to avoid creating
+# duplicates.
 ###
 def add_meetings(config, http_client)
-	group_id = get_group_id(config, http_client)
+	meeting_page_document = get_meeting_page_document(config, http_client)
+	group_id = get_group_id(meeting_page_document)
 	repeat_dates = get_repeat_dates(config)
 	repeat_dates.each do |meeting_date|
 		display_date = meeting_date.strftime(DISPLAY_DATE_FORMAT)
-		puts "Adding #{display_date} meeting…"
-		start_date_string = get_ch_date(config, meeting_date, 'start_time')
-		end_date_string = get_ch_date(config, meeting_date, 'end_time')
-		args = { 'runnum' => '2', 'rm' => 'scheduleeditor', 'groupid' => group_id, 'eventid' => '', 'startdate' => start_date_string, 'enddate' => end_date_string, 'location' => config['location_string'], 'latitude' => config['latitude'], 'longitude' => config['longitude'], 'locationdetails' => config['meeting_details'], 'newmeeting' => 'Create New Meeting...' } 
-		http_client.post(URL, args)
+		if get_meeting_date_found(config, meeting_page_document, meeting_date)
+			puts "Meeting for #{display_date} already found."
+		else
+			puts "Adding #{display_date} meeting…"
+			start_date_string = get_ch_input_date(config, meeting_date, 'start_time')
+			end_date_string = get_ch_input_date(config, meeting_date, 'end_time')
+			args = { 'runnum' => '2', 'rm' => 'scheduleeditor', 'groupid' => group_id, 'eventid' => '', 'startdate' => start_date_string, 'enddate' => end_date_string, 'location' => config['location_string'], 'latitude' => config['latitude'], 'longitude' => config['longitude'], 'locationdetails' => config['meeting_details'], 'newmeeting' => 'Create New Meeting...' } 
+			response = http_client.post(URL, args)
+			response_document = Oga.parse_html response.content
+			if !get_meeting_date_found(config, response_document, meeting_date) 
+				puts "Should have added a meeting for #{display_date} but did not find one in the resulting document. Stopping."
+				exit
+			end
+		end
 	end
 end
 
+###
+# Main body
+###
 config = YAML.load_file('config.yaml')
 http_client = HTTPClient.new
 display_confirmation(config)
@@ -139,5 +170,3 @@ password = prompt_for_password(config)
 login(config, http_client, password)
 add_meetings(config, http_client)
 puts "Done."
-puts
-puts "Important: This script has no way to validate that the meetings are added. Please verify through the user interface."
